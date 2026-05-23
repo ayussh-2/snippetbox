9e1e22b0fb816746ce5001e7a3bbf36bb4cd22bb
ayush <ayushatschool5@gmail.com>
2026-05-23 19:33:09 +0530

    refactor: add a custom logger and added flags detection

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,18 +1,44 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+}
+
 func main() {
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
 	mux := http.NewServeMux()
 	fileserver := http.FileServer(http.Dir("./ui/static"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileserver))
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet/view", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
-	log.Print("Starting Server on port: 4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+
+	app := &application{
+		errorLog: errorLog,
+		infoLog:  infoLog,
+	}
+
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet/view", app.snippetView)
+	mux.HandleFunc("/snippet/create", app.snippetCreate)
+	addr := flag.String("addr", ":4000", "Http Network Address: ")
+	flag.Parse()
+
+	infoLog.Printf("Starting Server on port: %s", *addr)
+
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog, Handler: mux,
+	}
+
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
